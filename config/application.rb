3919c58bcc587dbc5529260b037d867121d4ffc0
defektive <defektive@users.noreply.github.com>
2014-07-14 14:51:41 -0600

    Setup route constraints for the api
    Lots of changes, supposedly a versioned api

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module MarketPlaceApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -26,5 +28,17 @@ module MarketPlaceApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+        config.generators do |g|
+            g.test_framework :rspec, fixture: true
+            g.fixture_replacement :factory_girl, dir: 'spec/factories'
+            g.view_specs false
+            g.helper_specs false
+            g.stylesheets = false
+            g.javascripts = false
+            g.helper = false
+        end
+
+        config.autoload_paths += %W(\#{config.root}/lib)
+
   end
 end
